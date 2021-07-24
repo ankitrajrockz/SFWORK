a7a4db1ea457e02f8c9a9322b3f02dd9ece04970
ankitrajrockz <49672446+ankitrajrockz@users.noreply.github.com>
2021-07-24 18:12:20 +0530

    peopertyowner

--- force-app/main/default/aura/MyPropertyResult/MyPropertyResultController.js
+++ force-app/main/default/aura/MyPropertyResult/MyPropertyResultController.js
@@ -1,5 +1,23 @@
 ({
     fecthAllProp : function(component, event, helper) {
         helper.fecthAllProp(component, event, helper);
+    },
+    ownerDetails : function(component, event, helper) {
+        var eventSource= event.getSource();
+        var propOwner=eventSource.get('v.value');
+        
+        component.set('v.propownerId',propOwner);
+        component.set('v.openOwnerDetails',"true");
+        
+        
+    },
+    feedbackClk : function(component, event, helper) {
+        
+    },
+    propertyDetails : function(component, event, helper) {
+        
+    },
+    closeOwnerModel : function(component, event, helper) {
+        component.set('v.openOwnerDetails',false);
     }
 })
